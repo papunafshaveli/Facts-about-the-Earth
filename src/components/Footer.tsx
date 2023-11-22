c4bf878acbe99b0a7700f90412cbc4c1a948dd09
Papuna Fshaveli <papunafshaveli@gmail.com>
2023-11-22 19:30:53 +0400

    edit footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,39 @@
 import styled from "styled-components";
-
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 const FooterDiv = styled.div`
   position: absolute;
   bottom: 0;
+
   width: 100%;
   height: 5rem;
-  background-color: red;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  background-color: #386592;
 `;
 
+const handleFbClick = () => {
+  window.location.href = "https://www.facebook.com/papunafshaveli";
+};
+
+const handleInstaClick = () => {
+  window.location.href = "https://www.instagram.com/papunart/";
+};
+
+const handleYtClick = () => {
+  window.location.href =
+    "https://www.youtube.com/channel/UC8GUnP1KWNGgMZnKR7dLy-w";
+};
 const Footer = () => {
-  return <FooterDiv>Footer</FooterDiv>;
+  return (
+    <FooterDiv>
+      <FaFacebook onClick={handleFbClick} />
+      <FaInstagram onClick={handleInstaClick} />
+      <FaYoutube onClick={handleYtClick} />
+    </FooterDiv>
+  );
 };
 
 export default Footer;
